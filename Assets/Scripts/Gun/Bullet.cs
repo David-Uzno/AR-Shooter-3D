8bb83aab20807c52a35912eb67f9d6b829794a8e
David <141585041+David-Uzno@users.noreply.github.com>
2025-04-11 15:08:08 -0300

    feat: Disparo y Creación de Balas

---
+++ Assets/Scripts/Gun/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _shootingTimeLife = 1f;
+    [SerializeField] private float _impactTimeLife = 0.25f;
+
+    private void Start()
+    {
+        Destroy(gameObject, _shootingTimeLife);
+    }
+
+    /*void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Enemy"))
+        {
+			if (collider.gameObject.GetComponent<Enemy>() != null)
+			{
+				collider.gameObject.GetComponent<Enemy>().ReduceLife();
+			}
+            Destroy(gameObject, _impactTimeLife);
+        }
+    }*/
+}
---
+++ Assets/Scripts/Gun/Shooting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Shooting : MonoBehaviour
+{
+    [Header("Fire")]
+    [SerializeField] private float _shotForce = 10f;
+    [SerializeField] private float _shotCoolDown = 0.5f;
+    private float _shotRateTime = 0;
+
+	[Header("Dependencies")]
+	[SerializeField] private PlayerInput _playerInput;
+	[SerializeField] private Transform _firePoint;
+    [SerializeField] private GameObject _bulletPrefab;
+
+    private void Update()
+    {
+        Fire();
+    }
+
+	private void Fire()
+	{
+		if (_playerInput.actions["Attack"].ReadValue<float>() > 0)
+		{
+			if (Time.time > _shotRateTime)
+			{
+				// Instanciar Bala
+				GameObject newBullet = Instantiate(_bulletPrefab, _firePoint.position, Quaternion.identity);
+				Rigidbody _rigidbody = newBullet.GetComponent<Rigidbody>();
+
+				// Fuerza de Bala
+				if (_rigidbody != null)
+				{
+					_rigidbody.AddForce(transform.forward * _shotForce, ForceMode.Impulse);
+				}
+
+				// Tiempo de Cooldown
+				_shotRateTime = Time.time + _shotCoolDown;
+			}
+		}
+	}
+}
