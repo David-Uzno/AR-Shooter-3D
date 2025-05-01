6302a43fd86c4d867d5d051b1cd6a98234300f48
David <141585041+David-Uzno@users.noreply.github.com>
2025-05-01 18:18:11 -0300

    refactor: Limite para la Verificación del Giroscopio

--- Assets/Scripts/Characters/Player/Gyroscope.cs
+++ Assets/Scripts/Characters/Player/Gyroscope.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class Gyroscope : MonoBehaviour
 {
     private UnityEngine.Gyroscope _gyroscope;
-
-    void Start()
+    private int _verificationCount = 0;
+    [SerializeField] private byte _verificationAttempts = 10;
+    private bool _isGyroscopeSupported = false;
+    
+    private void Start()
     {
         StartCoroutine(EnsureGyroscopeEnabledCoroutine());
     }
@@ -16,25 +19,34 @@ public class Gyroscope : MonoBehaviour
     {
         while (_gyroscope == null || !_gyroscope.enabled)
         {
-            Debug.Log("Verificando giroscopio");
+            Debug.Log("Verificando giroscopio.");
 
             if (SystemInfo.supportsGyroscope)
             {
                 _gyroscope = Input.gyro;
                 _gyroscope.enabled = true;
+                _isGyroscopeSupported = true;
             }
             else
             {
-                Debug.LogError("Giroscopio no soportado.");
-                yield break;
+                _verificationCount++;
+                if (_verificationCount >= _verificationAttempts)
+                {
+                    Debug.LogError("Giroscopio no soportado.");
+                    _isGyroscopeSupported = false;
+                    this.enabled = false;
+                    yield break;
+                }
             }
 
             yield return new WaitForSeconds(0.2f);
         }
     }
 
-    void Update()
+    private void Update()
     {
+        if (!_isGyroscopeSupported) return;
+
         Quaternion adjustedRotation = new Quaternion(-_gyroscope.attitude.x, -_gyroscope.attitude.y, _gyroscope.attitude.z, _gyroscope.attitude.w);
         Vector2 eulerAngles = (Quaternion.Euler(90, 0, 0) * adjustedRotation).eulerAngles;
         transform.rotation = Quaternion.Euler(eulerAngles.x, eulerAngles.y, 0);
