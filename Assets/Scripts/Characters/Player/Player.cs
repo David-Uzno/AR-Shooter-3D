97c4eab4220f5de2369a3c3470091d90b0ce55da
David <141585041+David-Uzno@users.noreply.github.com>
2025-07-16 17:24:01 -0300

    feat: Barra de Vida Funcional

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -1,17 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    [Header("Status")]
     [SerializeField] private byte _life = 10;
+    private byte _lifeMax;
+    private byte _lifeCurrent;
+
+    [Header("Dependencies")]
+    [SerializeField] private Image _lifeBarUI;
+
     private int _score = 0;
+
+    private void Start()
+    {
+        _lifeMax = _life;
+        _lifeCurrent = _lifeMax;
+    }
 
     public void TakeDamage(byte damage)
     {
-        _life -= damage;
-        if (_life <= 0)
+        _lifeCurrent -= damage;
+
+        if (_lifeBarUI != null)
         {
-            Debug.Log("Player has died");
+            UpdateLifeBar();
         }
+
+        if (_lifeCurrent <= 0)
+        {
+            Debug.Log("Muerte");
+        }
+    }
+
+    private void UpdateLifeBar()
+    {
+        _lifeBarUI.fillAmount = (float)_lifeCurrent / _lifeMax;
     }
 
     public void AddPoints(int points)
