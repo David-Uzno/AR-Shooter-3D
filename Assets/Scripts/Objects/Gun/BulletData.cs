26c08922cc417c2fc83957046844f50865e01e77
David <141585041+David-Uzno@users.noreply.github.com>
2026-03-09 21:42:04 -0300

    feat: layers para filtrar los impactos de las balas

--- Assets/Scripts/Objects/Gun/BulletData.cs
+++ Assets/Scripts/Objects/Gun/BulletData.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewBulletData", menuName = "AR-Shooter/Objects/Bullet Data")]
 public class BulletData : ScriptableObject
 {
+    [Header("Stats")]
     [SerializeField] private float _timeLife = 1.5f;
     [SerializeField] private int _damage = 1;
+
+    [Header("Collision")]
+    [SerializeField] private LayerMask _hitLayers = ~0;
+    public LayerMask HitLayers => _hitLayers;
     public float TimeLife => _timeLife;
     public int Damage => _damage;
 }
